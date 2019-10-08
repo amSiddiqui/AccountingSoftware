a1e9e7437a5add071be984d199ad097202c0e73d
Aamir <gt_ams@yahoo.in>
2019-10-08 17:00:25 +0530

    add company and accountant creation page

--- client/routes/company.js
+++ client/routes/company.js
@@ -3,8 +3,20 @@ const express = require('express');
 const router = express.Router();
 
 
+router.get('/accountant/create', (req, res, next) => {
+    res.render('company/createAccountant', {
+        accountant: 'Head',
+        countryCode: ['+91', '+88', '+65'],
+        email: "gt_ams@yahoo.in"
+    });
+});
+
 router.get('/create', (req, res, next) => {
-    res.render('company/create');
+    res.render('company/createCompany', {
+        countryCode: ['+91', '+88', '+65'],
+        currency: ['$ (USD)', '€ (EUR)', '£ (GBP)', '₹ (INR)'],
+        dateFormat: ['dd/mm/yyy', 'mm/dd/yyyy', 'yyyy/mm/dd']
+    });
 });
 
 module.exports = router;
