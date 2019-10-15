e64a61a9cfe6ee4e84c2b2aa0a27c2cb06dc6907
Aamir <gt_ams@yahoo.in>
2019-10-15 12:35:18 +0530

    add dashboard and company edit page

--- client/routes/company.js
+++ client/routes/company.js
@@ -4,7 +4,15 @@ const router = express.Router();
 
 
 router.get('/accountant/create', (req, res, next) => {
-    res.render('company/create', {
+    res.render('company/accountant/create', {
+        accountant: 'Head',
+        countryCode: ['+91', '+88', '+65'],
+        email: "gt_ams@yahoo.in"
+    });
+});
+
+router.get('/accountant/edit', (req, res, next) => {
+    res.render('company/accountant/edit', {
         accountant: 'Head',
         countryCode: ['+91', '+88', '+65'],
         email: "gt_ams@yahoo.in"
@@ -12,7 +20,32 @@ router.get('/accountant/create', (req, res, next) => {
 });
 
 router.get('/create', (req, res, next) => {
-    res.render('company/accountant/create', {
+    res.render('company/create', {
+        countryCode: ['+91', '+88', '+65'],
+        currency: ['$ (USD)', '€ (EUR)', '£ (GBP)', '₹ (INR)'],
+        dateFormat: ['dd/mm/yyy', 'mm/dd/yyyy', 'yyyy/mm/dd']
+    });
+});
+
+router.get('/edit', (req, res, next) => {
+    pseudoCompany = {
+        name: 'Pineapple',
+        countryCode: '+88',
+        phone: '8528606977',
+        email: 'contact@pineapple.co',
+        address1: '196 Temple Drive',
+        city: 'Dublin',
+        state: 'RI',
+        country: 'Ireland',
+        pincode: '226013',
+        currency: '£ (GBP)',
+        datefmt: 'mm/dd/yyyy',
+        taxrate: '3.14'
+    };
+
+
+    res.render('company/edit', {
+        company: pseudoCompany,
         countryCode: ['+91', '+88', '+65'],
         currency: ['$ (USD)', '€ (EUR)', '£ (GBP)', '₹ (INR)'],
         dateFormat: ['dd/mm/yyy', 'mm/dd/yyyy', 'yyyy/mm/dd']
