3fb098ecc170193a91af49349b8bf70f6fc6fa81
AJSTINGER98 <amann.jha@mail.com>
2019-10-21 15:22:37 +0530

    move vendor page to vendor dir

--- client/routes/vendor.js
+++ client/routes/vendor.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.render('vendor');
+    res.render('vendor/vendor');
 });
 
 router.get('/create', (req, res, next) => {
