3807e1b4c7ac54d4a13a3ee61bd752dead7dfa90
amitsingh19975 <amitsingh19975@gmail.com>
2019-11-11 01:15:46 +0530

    add routes

--- database/Application/urls.py
+++ database/Application/urls.py
@@ -8,7 +8,15 @@ app_name='Application'
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^init/$', views.init, name='init'),
-    # path('init/', views.init),
-    url(r'^auth/login$', views.login_user, name='login_user'),
-    url(r'^auth/logout$', views.logout, name='logout'),
+    url(r'^auth/login/$', views.login_user, name='login_user'),
+    url(r'^auth/logout/$', views.logout, name='logout'),
+    url(r'^auth/exists/$', views.user_exists, name='user_exists'),
+    url(r'^auth/accountant/exists/$', views.accountant_exists, name='accountant_exists'),
+    path('invoice/<int:invoice_id>/', views.fetch_invoice ),
+    path('invoice/create/', views.create_invoice ),
+    path('invoice/latest/', views.latest_invoice ),
+    path('invoice/delete/', views.delete_invoice ),
+    path('vendor/create/', views.create_vendor ),
+    path('vendor/', views.fetch_vendor)
+
 ]
