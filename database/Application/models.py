033e440edbf36c6fa33e3c06ce5184617c9ca742
Aamir <gt_ams@yahoo.in>
2019-11-13 00:47:06 +0530

    change invoice route

--- database/Application/models.py
+++ database/Application/models.py
@@ -12,7 +12,7 @@ class Currency(models.Model):
 	Id=models.AutoField(primary_key=True)
 	Code=models.CharField(max_length=10)
 	Name=models.CharField(max_length=50)
-	Symbol=models.CharField(max_length=10)
+	Symbol=models.CharField(max_length=10, default="")
 	
 
 #In mysql client use 'alter table application_phonecode AUTO_INCREMENT=1;' to have starting value as 1
