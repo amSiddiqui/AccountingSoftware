331b5870d67dcc022ad44f1e152b6fb2462a3fd5
AJSTINGER98 <amann.jha@gmail.com>
2019-11-18 00:43:42 +0530

    finalize report-page design

--- client/routes/report.js
+++ client/routes/report.js
@@ -22,35 +22,32 @@ router.get('/:type', (req , res, next) => {
   type = type.replace('-', ' ');
   util.authCheck(req , user =>{
     if(user){
-
+      var currency = user.company.currency;      
       let inv = axios.post(config.url + '/invoice/latest/',{
         accessToken:accessToken,
         token: user.token,
-        quantity: 5,
+        quantity: 15,
+        datefmt: user.company.datefmt,
       });
-
+      
       let expen = axios.post(config.url + '/expense/latest/',{
         accessToken:accessToken,
         token: user.token,
-        quantity: 5,
+        quantity: 15,
+        datefmt: user.company.datefmt,
       });
-
+      
       let clnt = axios.post(config.url + '/client/latest/',{
         accessToken:accessToken,
         token: user.token,
-        quantity: 5,
+        quantity: 15,
+        datefmt: user.company.datefmt,
       });
-      var currency = user.company.currency;
-      axios.all([inv,expen,clnt]).then(axios.spread((...res) => {
-        // const resoR = res[0].data.;
-        // const resoD = res[1];
-        // const resprof = res[2];
-        // const resrev = res[3];
-        // const resexp = res[4];
-        const resinv = res[0].data.invoices;
-        const resexpen = res[1].data.expenses;
-        const resclnt = res[2].data.clients;
-        console.log(resinv);
+      axios.all([inv,expen,clnt]).then(axios.spread((...response) => { 
+
+        const resinv = response[0].data.invoices;
+        const resexpen = response[1].data.expenses;
+        const resclnt = response[2].data.clients;
         var totalInvoiced =0;
         var totalPaid =0;
         var totalDue = 0;
@@ -68,6 +65,7 @@ router.get('/:type', (req , res, next) => {
             totalInvoiced: totalInvoiced,
             totalPaid: totalPaid,
             totalDue: totalDue,
+            company: user.company.name,
           });
         }
         else if(type == "CLIENT REPORT"){
@@ -75,7 +73,7 @@ router.get('/:type', (req , res, next) => {
           for(i in resclnt){
             total += resclnt[i].stats.total;
           }
-          res.render('report/show', {type: type, client: resclnt,total:total,currency: user.company.currency,});
+          res.render('report/show', {type: type, client: resclnt,total:total,currency: user.company.currency,company: user.company.name,});
         }
         else if(type == "PAYMENT PENDING"){
           totalDue = 0;
@@ -87,6 +85,7 @@ router.get('/:type', (req , res, next) => {
             invoice: resinv,
             currency: user.company.currency,
             total: totalDue,
+            company: user.company.name,
           });
         }
 
@@ -100,24 +99,123 @@ router.get('/:type', (req , res, next) => {
             invoice: resinv,
             currency: user.company.currency,
             total: totalPaid,
+            company: user.company.name,
           });
         }
         // TODO: fetch all clients from database associated with a particular item
-        // else if(type == "ITEM SOLD"){
-          //
-          //   res.render('report/show', {type: type, pseudoItem: seeds.pseudoItem});
-          // }
+        else if(type == "ITEM SOLD"){
+          console.log(user.company)
+          var pseudoItem = [];
+          var invID;
+          var invDate;
+          var fname;
+          var lname;
+          var c_ID;
+          var temp= {};
+          var detailsTemp = {};
+          let itId = -1;
+          var totalUnits =0;
+          var totalAmount=0;
+          resinv.forEach( inv =>{
+            invID = inv.id;
+            invDate = inv.date;
+            fname = inv.client.firstName;
+            lname = inv.client.lastName;
+            c_ID = inv.client.id;
+            inv.items.forEach(items=>{
+              totalUnits += items.quantity;
+              totalAmount += items.price;
+              if(pseudoItem.length == 0){
+                temp = {
+                  Item_Id: items.Item_Id,
+                  Name: items.Name,
+                  Description: items.Description,
+                  Rate: items.Rate,
+                  details: [],
+                  totalQuantity: 0,
+                  totalPrice:0,
+                };
+                detailsTemp = {
+                  quantity: items.quantity,
+                  price: items.price,
+                  invoiceID: invID,
+                  invoiceDate:invDate,
+                  clientID : c_ID,
+                  clientName: {firstName:fname,lastName:lname},
+
+                };
+                temp.totalQuantity = detailsTemp.quantity;
+                temp.totalPrice = detailsTemp.price;
+                temp.details.push(detailsTemp);
+                pseudoItem.push(temp);
+                }
+                else{
+                  if(pseudoItem.some(itm => itm.Item_Id === items.Item_Id)){
+                    detailsTemp = {
+                      quantity: items.quantity,
+                      price: items.price,
+                      invoiceID: invID,
+                      invoiceDate:invDate,
+                      clientID : c_ID,
+                      clientName: {firstName:fname,lastName:lname},
+                    };
+                    for(var i in pseudoItem){
+                      if(pseudoItem[i].Item_Id === items.Item_Id){
+                        pseudoItem[i].details.push(detailsTemp);
+                        pseudoItem[i].totalQuantity += detailsTemp.quantity;
+                        pseudoItem[i].totalPrice += detailsTemp.price;
+                        break;
+                      }
+                    }
+                  }
+                  else{
+                    temp = {
+                      Item_Id: items.Item_Id,
+                      Name: items.Name,
+                      Description: items.Description,
+                      Rate: items.Rate,
+                      details: [],
+                      totalQuantity: 0,
+                      totalPrice:0,
+                    };
+                    detailsTemp = {
+                      quantity: items.quantity,
+                      price: items.price,
+                      invoiceID: invID,
+                      invoiceDate:invDate,
+                      clientID : c_ID,
+                      clientName: {firstName:fname,lastName:lname},
+                    };
+                    temp.totalQuantity = detailsTemp.quantity;
+                    temp.totalPrice = detailsTemp.price;
+                    temp.details.push(detailsTemp);
+                    pseudoItem.push(temp); 
+                  }
+                }
+              });
+            });
+            res.render('report/show', {
+              type: type, 
+              pseudoItems: pseudoItem, 
+              currency: user.company.currency,
+              totalUnits: totalUnits,
+              totalAmount:totalAmount,
+              company: user.company.name, 
+            });
+        }
 
           else if(type == 'EXPENSE REPORT'){
             total =0;
+            
             for(i in resexpen){
-              total += resexpen[i].subtotal;
+              total += resexpen[i].amount;
             }
             res.render('report/show', {
               type: type,
               expense: resexpen,
               currency: currency,
               total:total,
+              company: user.company.name,
             });
           }
           else if(type == 'PROFIT LOSS'){
@@ -135,10 +233,12 @@ router.get('/:type', (req , res, next) => {
             res.render('report/show',{
               type: type,
               expense: resexpen,
+              invoice: resinv,
               salesTotal:salesTotal,
               expenseTotal:expenseTotal,
               profit:profit,
               currency: user.company.currency,
+              company: user.company.name,
             });
           }
           // else if(type === 'GENERAL LEDGER'|| type === 'BALANCE SHEET'|| type === 'TRIAL BALANCE'){
